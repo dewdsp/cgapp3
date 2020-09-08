50ef57c181fa4d1c6f3e1e99523041f34edeb1d7
Sitthakarn <sitthakarn.p@gmail.com>
2020-09-08 20:57:33 +0700

    init app3

--- backend/pkg/apiserver/routes.go
+++ backend/pkg/apiserver/routes.go
@@ -8,30 +8,33 @@ func Routes(app *fiber.App) {
 	APIGroup := app.Group("/api")
 
 	// API routes
-	APIGroup.Get("/docs", func(c *fiber.Ctx) {
-		// Set JSON data
-		data := fiber.Map{
-			"message": "ok",
-			"results": []fiber.Map{
-				{
-					"name": "Documentation",
-					"url":  "https://create-go.app/",
-				},
-				{
-					"name": "Detailed guides",
-					"url":  "https://create-go.app/detailed-guides/",
-				},
-				{
-					"name": "GitHub",
-					"url":  "https://github.com/create-go-app/cli",
-				},
-			},
-		}
+	APIGroup.Get("/docs", GetDocs)
+}
 
-		// Set 200 OK status and return JSON, or skip route
-		if errJSON := c.Status(200).JSON(data); errJSON != nil {
-			// Send 500 status and error to Fiber
-			c.Status(500).Next(errJSON)
-		}
-	})
+// GetDocs will send the data of documents
+func GetDocs(ctx *fiber.Ctx) {
+	// Set JSON data
+	data := fiber.Map{
+		"message": "ok",
+		"results": []fiber.Map{
+			{
+				"name": "Documentation",
+				"url":  "https://create-go.app/",
+			},
+			{
+				"name": "Detailed guides",
+				"url":  "https://create-go.app/detailed-guides/",
+			},
+			{
+				"name": "GitHub",
+				"url":  "https://github.com/create-go-app/cli",
+			},
+		},
+	}
+
+	// Set 200 OK status and return JSON, or skip route
+	if errJSON := ctx.Status(200).JSON(data); errJSON != nil {
+		// Send 500 status and error to Fiber
+		ctx.Status(500).Next(errJSON)
+	}
 }
