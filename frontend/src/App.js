00ca4b00b8ace2d3e8feb73cb4f5d8419278b256
Sitthakarn <sitthakarn.p@gmail.com>
2020-09-10 23:23:27 +0700

    frontend: add Person.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Now is on React.</h1>
+        <p>This is really working...</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My hobbies: Racing</Person>
+        <Person name="Dew" age="48"/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by Dew
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+    // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Hi, I \'m React app.'));
+  }
 }
 
 export default App;
