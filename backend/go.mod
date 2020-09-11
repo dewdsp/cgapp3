377abfd42c9495005ecbee916e92a20b4300836c
dewdsp <sitthakarn.p@gmail.com>
2020-09-12 00:20:44 +0700

    use state in js

--- backend/go.mod
+++ backend/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gofiber/cors v0.2.2
 	github.com/gofiber/fiber v1.14.5
 	github.com/stretchr/testify v1.6.1
+	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 30},
+      { name: 'Manu', age: 29},
+      { name: 'Stephane', age: 26}
+    ],
+    otherState: 'some other state'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // Don't Do THIS: this.state.persons[0].name = 'Dew';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 30},
+        { name: 'Manu', age: 34},
+        { name: 'Stephane', age: 27}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello, Now is on React.</h1>
         <p>This is really working...</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My hobbies: Racing</Person>
-        <Person name="Dew" age="48"/>
-
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Hi, I \'m React app.'));
